f542c64623637c4043ab2deae0f859a00562be55
DrewGamer <arsailor89@yahoo.com>
2017-10-05 12:02:49 -0400

    Grid/Star Background and Test Scene
    Created a basic star background with a grid overlay for a test scene.
    Basic object with basic movement added for testing.

---
+++ ProjectZP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotZ = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, 0, rotZ);
+        transform.Translate(0, y, 0);
+
+		
+	}
+}
