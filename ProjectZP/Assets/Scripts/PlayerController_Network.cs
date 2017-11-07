ca9d44db01139705d275066f72fd6a5b0e1376f4
Andrew Ruegamer <arsailor89@yahoo.com>
2017-11-07 17:23:38 -0500

    basic networking and .gitignore working
    everything should be good now (for real real)

---
+++ ProjectZP/Assets/Scripts/PlayerController_Network.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController_Network : NetworkBehaviour {
+
+    public GameObject projectile;
+    public float speed;
+    public float moveLimit;
+
+    public GameObject movementSphere;
+
+    private bool isMoving;
+    private float distance;
+    private Vector3 targetPos;
+    private Vector3 mousePos;
+
+	// Use this for initialization
+	void Start () {
+        movementSphere = Instantiate(movementSphere, gameObject.transform);
+
+        if (!isLocalPlayer)
+            movementSphere.GetComponent<Renderer>().enabled = false;
+	}
+
+    void OnMouseUp()
+    {
+        if (!isLocalPlayer)
+            return;
+        Debug.Log("Click");
+
+        MoveSelectedUnitTo();
+    }
+
+    public void MoveSelectedUnitTo()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = -1;
+        distance = Vector3.Distance(transform.position, targetPos);
+        movementSphere.transform.localScale -= new Vector3(distance * 2, distance * 2, 0);
+        Debug.Log("Distance" + distance);
+
+        isMoving = true;
+
+        mousePos = Input.mousePosition;
+        Vector3 currentPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        mousePos.x = mousePos.x - currentPos.x;
+        mousePos.y = mousePos.y - currentPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        
+        if (Input.GetKey("space"))
+        {
+            movementSphere.transform.localScale = new Vector3(moveLimit, moveLimit, 0.1f);
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        if (isMoving && distance > 0.01)
+        {
+            distance = Vector3.Distance(transform.position, targetPos);
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * speed);
+        }
+        else if (isMoving && distance < 0.01 && distance > 0)
+            transform.position = targetPos;
+        else
+            isMoving = false;
+    }
+}
