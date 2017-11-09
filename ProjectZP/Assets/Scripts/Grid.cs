04acfe222e2eef9ed7cbcfd3cc8c5aec22be3ed1
Adrian Brooks <brooksat@email.sc.edu>
2017-11-09 18:39:54 -0500

    update
    keyboard movement, clamping, occupation,

---
+++ ProjectZP/Assets/Scripts/Grid.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public Tile space;
+    public int numberOfTiles = 100 ;
+    public int tilePerRow = 10;
+    public float distanceBetweenTiles = 1.0f;
+    public int bClamp = 1;
+    public int tClamp = 11;
+
+    public Tile[,] tiles = new Tile[10,10];
+
+	// Use this for initialization
+	void Start () {
+        CreateTiles();
+	}
+
+    void CreateTiles()
+    {
+        float xOffset = 1.0f;
+        float yOffset = 1.0f;
+
+
+        for(int i = 0; i < numberOfTiles; i++)
+        {
+            xOffset += distanceBetweenTiles;
+
+            
+            if (i % tilePerRow == 0)
+            {
+                if (i > 0)
+                {
+                    yOffset += distanceBetweenTiles;
+                }
+                xOffset = 1;
+            }
+
+          tiles[(int)xOffset-1,(int)yOffset-1] = Instantiate(space, new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z ), transform.rotation);
+  
+        }
+    }
+	
+	// Update is called once per frame
+	
+}
