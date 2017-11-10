0500ae1ede98bc6369d359c94d9ce9823e7101be
Adrian Brooks <brooksat@email.sc.edu>
2017-11-10 07:07:49 -0500

    patch
    energyWave animation

---
+++ ProjectZP/Assets/Scripts/EnergyWave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyWave : MonoBehaviour {
+
+    private float lifeTime;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+
+        
+}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localScale += new Vector3(0.2f, 0.2f, 0);
+
+        Color color = gameObject.GetComponent<Renderer> ().material.color;
+        color.a -= 0.1f;
+        gameObject.GetComponent<Renderer>().material.color = color;
+
+        if (lifeTime > 40)
+        {
+            //Destroy(gameObject);
+            
+            
+            gameObject.SetActive(false);
+            color.a = 1.0f;
+            gameObject.GetComponent<Renderer>().material.color = color;
+            gameObject.transform.localScale = new Vector3(1, 1, 0.5f);
+            lifeTime = 0;
+        }
+        else lifeTime++;
+
+    }
+}
--- ProjectZP/Assets/Scripts/PlayerController_Network.cs
+++ ProjectZP/Assets/Scripts/PlayerController_Network.cs
@@ -4,6 +4,8 @@ using UnityEngine.Networking;
 public class PlayerController_Network : NetworkBehaviour {
 
     public GameObject projectile;
+    public GameObject energyWave;
+    public bool eWave = false;
     public float speed;
     public float moveLimit;
 
@@ -13,22 +15,48 @@ public class PlayerController_Network : NetworkBehaviour {
     private float distance;
     private Vector3 targetPos;
     private Vector3 mousePos;
+    private Vector3 direction;
+
+
+
 
 	// Use this for initialization
 	void Start () {
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        //Makes movement sphere
         movementSphere = Instantiate(movementSphere, gameObject.transform);
 
+        //Cannot see other ships movement range
         if (!isLocalPlayer)
+=======
+        if (!isLocalPlayer) {
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+        if (!isLocalPlayer) {
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+        if (!isLocalPlayer) {
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+        if (!isLocalPlayer) {
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
             movementSphere.GetComponent<Renderer>().enabled = false;
+        }
 	}
 
     void OnMouseUp()
     {
         if (!isLocalPlayer)
             return;
-        Debug.Log("Click");
 
-        MoveSelectedUnitTo();
+        if (WhichTurn.playerturn == 1)
+        {
+            Debug.Log("Click");
+            MoveSelectedUnitTo();
+        }
     }
 
     public void MoveSelectedUnitTo()
@@ -36,23 +64,107 @@ public class PlayerController_Network : NetworkBehaviour {
         if (!isLocalPlayer)
             return;
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        //transforms position of mouse from screen space into world space.
         targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         targetPos.z = -1;
+        //distance set to distance of this object to distance of target position
         distance = Vector3.Distance(transform.position, targetPos);
+        //changes size of movements sphere based on distance traveled
         movementSphere.transform.localScale -= new Vector3(distance * 2, distance * 2, 0);
         Debug.Log("Distance" + distance);
+=======
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = -1;
+            distance = Vector3.Distance(transform.position, targetPos);
+            movementSphere.transform.localScale -= new Vector3(distance * 2, distance * 2, 0);
+            Debug.Log("Distance" + distance);
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 
-        isMoving = true;
+            isMoving = true;
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
 
+            isMoving = true;
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+
+            mousePos = Input.mousePosition;
+            Vector3 currentPos = Camera.main.WorldToScreenPoint(transform.position);
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+        //mouse position set to current mouse location(in pixel coordinates)
         mousePos = Input.mousePosition;
+        //current position of gameobject
         Vector3 currentPos = Camera.main.WorldToScreenPoint(transform.position);
 
+        //used for calculating angle of ship
         mousePos.x = mousePos.x - currentPos.x;
         mousePos.y = mousePos.y - currentPos.y;
+=======
+            mousePos.x = mousePos.x - currentPos.x;
+            mousePos.y = mousePos.y - currentPos.y;
 
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
 
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+<<<<<<< HEAD
+        //changes direction ship is facing on click
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+
+            isMoving = true;
+
+            mousePos = Input.mousePosition;
+            Vector3 currentPos = Camera.main.WorldToScreenPoint(transform.position);
+
+            mousePos.x = mousePos.x - currentPos.x;
+            mousePos.y = mousePos.y - currentPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+
+            isMoving = true;
+
+            mousePos = Input.mousePosition;
+            Vector3 currentPos = Camera.main.WorldToScreenPoint(transform.position);
+
+            mousePos.x = mousePos.x - currentPos.x;
+            mousePos.y = mousePos.y - currentPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+            mousePos.x = mousePos.x - currentPos.x;
+            mousePos.y = mousePos.y - currentPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
     }
 
     // Update is called once per frame
@@ -61,14 +173,28 @@ public class PlayerController_Network : NetworkBehaviour {
         if (!isLocalPlayer)
             return;
         
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        //resets movements sphere
         if (Input.GetKey("space"))
         {
             movementSphere.transform.localScale = new Vector3(moveLimit, moveLimit, 0.1f);
         }
-
+        //quits application
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
         if (Input.GetKey("escape"))
             Application.Quit();
 
+        //movement of ship
         if (isMoving && distance > 0.01)
         {
             distance = Vector3.Distance(transform.position, targetPos);
@@ -78,5 +204,98 @@ public class PlayerController_Network : NetworkBehaviour {
             transform.position = targetPos;
         else
             isMoving = false;
+
+        if (Input.GetKeyDown("b"))
+        {
+            projectile = Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation );
+            projectile.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown("n"))
+        {
+            if (!eWave)
+            {
+                energyWave = Instantiate(energyWave, gameObject.transform.position, gameObject.transform.rotation);
+                eWave = true;
+            }
+            energyWave.gameObject.SetActive(true);
+        }
+
     }
+
+
+    void OnGUI()
+    {
+
+        if (GUILayout.Button("Move"))
+        {
+            movementSphere = Instantiate(movementSphere, gameObject.transform);
+            movementSphere.transform.localScale = new Vector3(moveLimit, moveLimit, 0.1f);
+        }
+
+        if (GUILayout.Button("Attack"))
+        {
+
+        }
+
+        if(GUILayout.Button("End Turn"))
+        {
+
+        }
+    }
+
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+    void OnGUI()
+    {
+
+        if (GUILayout.Button("Move"))
+        {
+            movementSphere = Instantiate(movementSphere, gameObject.transform);
+            movementSphere.transform.localScale = new Vector3(moveLimit, moveLimit, 0.1f);
+        }
+
+        if (GUILayout.Button("Attack"))
+        {
+
+        }
+
+        if(GUILayout.Button("End Turn"))
+        {
+
+        }
+    }
+
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+    void OnGUI()
+    {
+
+        if (GUILayout.Button("Move"))
+        {
+            movementSphere = Instantiate(movementSphere, gameObject.transform);
+            movementSphere.transform.localScale = new Vector3(moveLimit, moveLimit, 0.1f);
+        }
+
+        if (GUILayout.Button("Attack"))
+        {
+
+        }
+
+        if(GUILayout.Button("End Turn"))
+        {
+
+        }
+    }
+
+
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
+=======
+>>>>>>> 221e4c0493f4ca6083e69305f39e3635ad2caa4b
 }
