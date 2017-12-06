39940f0aacacad3d13d9200058b302b4472297cd
Adrian Brooks <brooksat@email.sc.edu>
2017-12-05 23:13:50 -0500

    Fixed Menu Scene

---
+++ ProjectZP/Assets/Scripts/LoadTestScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadTestScene : MonoBehaviour {
+
+	public void LoadTest()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
